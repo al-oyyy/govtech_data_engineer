67a94e22b123fb9978819b4f9aca22dacb661e64
Aloysius Chen <aloysius1919@gmail.com>
2024-08-27 17:28:29 +0800

    edit data type of excel file to match json countryid type

--- assignment1.py
+++ assignment1.py
@@ -12,6 +12,12 @@ body_dict = response.json()
 #Reading Data from Country Code Excel File
 countries = pd.read_excel('Country-Code.xlsx')
 
+#Updated the type of the values in the countries data structure to be integer so that it matches with the type of
+#country id in the JSON provided - integer
+countries['Country Code'] = pd.to_numeric(countries['Country Code'],errors='coerce').astype(int)
+
+countries = pd.Series(countries['Country'].values, index = countries['Country Code']).to_dict()
+
 #Assigning the name for the eventual csv file and the respective column names
 #Open the CSV file used for the final output
 file_name = 'restaurants.csv'
@@ -28,7 +34,7 @@ with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
             row = {
                 'Restaurant Id': restaurant.get('id','NA'),
                 'Restaurant Name': restaurant.get('name','NA'),
-                'Country': countries.get(str(restaurant.get('location').get('country_code')).strip(),'NA'),
+                'Country': countries.get(int(restaurant.get('location').get('country_id',0)),'NA'),
                 'City': restaurant['location'].get('city','NA'),
                 'User Rating Votes': restaurant['user_rating'].get('votes','NA'),
                 'User Aggregate Rating': float(restaurant['user_rating'].get('aggregate_rating',0.0)),
@@ -38,8 +44,6 @@ with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
 
 
 print('restaurants.csv is successfully outputted!')
-
-###
 
 
 
